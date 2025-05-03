6450788a6dffb171a3ebdaac417e4a395def3014
Ryan Gabriel Togar Simamora <satupaduryan132@gmail.com>
2025-05-03 19:55:06 +0700

    feat : Menambahkan file notif_service.py

---
+++ service/notif_service.py
@@ -0,0 +1,24 @@
+from utils.file_manager import read_json
+from typing import List, Dict
+
+BARANG_PATH = "data/barang.json"
+
+def get_barang_menipis(id_barang: str) -> Dict:
+    # Fungsi untuk memeriksa barang dengan stok menipis
+    data = read_json(BARANG_PATH)
+    for kategori, daftar_barang in data.items():
+        for barang in daftar_barang:
+            if barang.get("id") == id_barang and barang.get("stok", 0) < 10:
+                return barang
+    return {}
+
+def get_semua_barang_menipis() -> List[Dict]:
+    data = read_json(BARANG_PATH)
+    barang_menipis = []
+
+    for kategori, daftar_barang in data.items():
+        for barang in daftar_barang:
+            if barang.get("stok", 0) < 10:
+                barang_menipis.append(barang)
+    
+    return barang_menipis
