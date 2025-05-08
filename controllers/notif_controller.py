57403a91051ea7cea704551053ccf2f3b4098a9b
zaenarifputra <zaenarifputraainurdin@gamil.com>
2025-05-08 15:40:05 +0700

    refactor: perbarui kesalahan import

--- controllers/notif_controller.py
+++ controllers/notif_controller.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 from service.notif_service import get_barang_menipis
 from service.notif_service import get_semua_barang_menipis
 
-router = APIRouter(prefix="/stok")
+router = APIRouter(prefix="/stok/stok")
 
 @router.get("/", summary="Notifikasi semua barang dengan stok menipis")
 def notif_stok_menipis():
