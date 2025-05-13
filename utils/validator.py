694c8b2b54e2bfbc5da56240013feaa86ca5375a
Zaenarif Putra <91659924+zaenarifputra@users.noreply.github.com>
2025-05-13 21:50:53 +0700

    Delete utils/validator.py

--- utils/validator.py
+++
@@ -1,8 +0,0 @@
-import re
-
-def validasi_stok(stok: int) -> bool:
-    return stok >= 0
-
-def valid_id_barang(id_barang: str) -> bool:
-    return bool(re.fullmatch(r"[A-Z]{1,2}\d{2,5}", id_barang))
-
