e7c8502d5918cb490d32ed4ca05d5ddbef58e9ad
Zaenarif Putra <91659924+zaenarifputra@users.noreply.github.com>
2025-05-13 19:55:42 +0700

    Delete schemas/barang_schema.py

--- schemas/barang_schema.py
+++
@@ -1,9 +0,0 @@
-# schemas/barang_schema.py
-from pydantic import BaseModel, Field
-
-class Barang(BaseModel):
-    id: str
-    nama: str
-    berat: str
-    harga: int
-    stok: int = Field(..., ge=0, description="Stok tidak boleh negatif")
