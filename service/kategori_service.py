f78417e29dd48a5ee67d11c33f80e9fd0833f919
rasunaaa <2311104043@ittelkom-pwt.ac.id>
2025-05-27 14:20:35 +0700

    feat : cleancode kategori service.py

--- service/kategori_service.py
+++ service/kategori_service.py
@@ -1,10 +1,7 @@
 # shafiq
-from utils.file_manager import read_json
 from typing import TypeVar, List
+from utils.file_manager import read_json
+T = TypeVar("T")
 
-
-T = TypeVar('T') 
-
-
-def get_all_kategori(data_path: str) -> List[T]:
-    return read_json(data_path)  # Ini akan mengembalikan tipe data yang ada dalam file JSON
+def get_all_items_from_json(data_path: str) -> List[T]:
+    return read_json(data_path)
