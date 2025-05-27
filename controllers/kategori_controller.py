c41928443ec4f102381cfaeeaee0b240789b6d3d
rasunaaa <2311104043@ittelkom-pwt.ac.id>
2025-05-27 14:22:21 +0700

    feat : cleancode kategori controller.py

--- controllers/kategori_controller.py
+++ controllers/kategori_controller.py
@@ -1,18 +1,28 @@
 # shafiq
 from fastapi import APIRouter
-from schemas.response import ResponseModel
-from service.kategori_service import get_all_kategori
 from typing import List, Dict
 
+from schemas.response import ResponseModel
+from service.kategori_service import get_all_kategori
 
 router = APIRouter()
 
-
-@router.get("/", response_model=ResponseModel[List[Dict]], summary="Tampilkan semua kategori")
+@router.get(
+    "/",
+    response_model=ResponseModel[List[Dict]],
+    summary="Tampilkan semua kategori",
+    tags=["Kategori"]
+)
 def tampilkan_semua_kategori():
-    result = get_all_kategori(data_path="data/kategori.json")
+    """
+    Endpoint untuk menampilkan semua data kategori.
+
+    Returns:
+        ResponseModel: Objek response berisi daftar kategori.
+    """
+    kategori_list = get_all_kategori(data_path="data/kategori.json")
     return ResponseModel(
         status="success",
         message="Kategori berhasil ditampilkan",
-        data=result
+        data=kategori_list
     )
